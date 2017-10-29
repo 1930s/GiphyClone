6182b66e53fa3bb0d23d07e1c102771e2ae9e21d
StefanSokic <stefan.sokic33@gmail.com>
2017-10-29 09:37:49 -0400

    first commit with proper API call, just dumps for now

---
+++ project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import axios from 'axios'
+
+const axiosconfig = {
+  baseurl: 'http://api.giphy.com/v1/gifs/trending?api_key=rr0lC3pBzUuNytwdumVXkT8njgPd0pbQ',
+  timeout: 30000,
+};
+let imageUrl = ''
+
+axios.get(axiosconfig.baseurl).then((response) => {
+    console.log(response)
+    imageUrl = response.data.data[0].url
+    console.log(imageUrl)
+})
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
